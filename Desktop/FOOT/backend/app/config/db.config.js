6f793da288db2928bd60dd3202091a0a82b64fd4
EHO GLORIA <ehogloria14@gmail.com>
2022-08-03 14:55:52 +0000

    API FTF

---
+++ Desktop/FOOT/backend/app/config/db.config.js
@@ -0,0 +1,15 @@
+//database credentials
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "FTFDB",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  PORT:3306,
+};
